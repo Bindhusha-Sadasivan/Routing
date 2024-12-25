109582dc87a8d39d5a90abfb1e87ae46ce008c2f
Bindhusha Sadasivan <bindhushasadasivan@gmail.com>
2024-12-25 08:53:31 +0530

    133 - Passing Parameters to Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
 import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
 
 export const routes: Routes = [
   {
@@ -10,6 +11,12 @@ export const routes: Routes = [
   {
     path:'users',
     component: UsersComponent
+  },
+  //Whatever we pass after our url => http://localhost:4200/users/hello or http://localhost:4200/users/hello, it will load UserComponent
+  // We should add : here. Then only it considers that as a parameter
+  {
+    path: 'users/:id',
+    component: UserComponent
   },
   {
     path: 'servers',
